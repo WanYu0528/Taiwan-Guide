dabaedc2841f11f7384083c1531f254d2e0c42a2
wanyu <melody90528@gmail.com>
2023-06-07 13:48:50 +0800

    fix: 修改顯示名稱錯誤

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -11,17 +11,18 @@ div(class="p-8")
   div(class="flex justify-around items-stretch flex-wrap mt-8")
     router-link(
       v-for="item in result" :key="item.ID"
-      :to="`/D/${item[mode + 'ID']}/`"
+      :to="`/D/${item[parm.mode + 'ID']}/`"
       class="flex flex-col w-[32%] pad:w-[47%] mobile:w-full rounded-lg overflow-hidden items-stretch mx-1 mb-4 bg-white transition hover:shadow-lg duration-500"
     )
-      div(class="w-full h-[250px] bg-[url('../images/logo.png')] bg-no-repeat bg-center transition transform duration-500 hover:scale-125")
+      div(class="w-full h-[250px] bg-[url('../assets/images/logo.png')] bg-no-repeat bg-center bg-contain")
         img(
-          class="w-full h-full object-center object-cover"
+          class="w-full h-full object-center object-cover transition transform duration-500 hover:scale-125"
           :src="item.Picture.PictureUrl1"
-          :alt="item.Picture.PictureDescription1 || item[mode + 'Name']"
+          :alt="item.Picture.PictureDescription1 || item[parm.mode + 'Name']"
+          onerror="this.style.display='none'"
         )
       div(class="flex-1 p-4 bg-white box-border")
-        h2(class="mb-4 text-xl font-bold") {{ item[mode + 'Name'] }}
+        h2(class="mb-4 text-xl font-bold") {{ item[parm.mode + 'Name'] }}
         p(
           v-if="item.Date"
           class="ml-5 text-base text-justify -indent-5 mobile:text-sm"
@@ -52,6 +53,12 @@ div(class="p-8")
         )
           i(class="icofont-location-pin mr-2")
           span {{ item.Location ? item.Location + " " + item.Address : item.Address }}
+  div(class="flex justify-center")
+    button(
+      v-show="!loadBtn"
+      @click="loadData()"
+      class="bg-gray-100 text-main rounded-xl mx-auto py-2 px-4 hover:bg-main hover:text-gray-100"
+    ) 載入更多
 </template>
 
 <script setup>
@@ -81,7 +88,7 @@ const loadData = async () => {
     let load = await tourism.ScenicSpot.gatCityTravelInfo(parm.mode, parm.city, parm.keyword, pageIdx);
     if (load.data.length === 0) throw new Error();
     if (load.data.length === 18) loadBtn.value = false;
-    result.value = load.data;
+    result.value = result.value.concat(load.data);
     console.log(result.value);
     pageIdx += 1;
     setTimeout(() => (loading.value = 1), 500);
@@ -93,7 +100,7 @@ const loadData = async () => {
 };
 const getTitle = () => {
   if (parm.keyword) return parm.keyword.split(",").join(" ");
-  else if (parm.city) return parm.city.replace(/[A-Z]/g, " $&");
+  else if (parm.city) return cityLib[parm.city].name;
   else return parm.mode;
 };
 loadData();
